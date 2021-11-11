4e07aa426de856811fbb0b418bb299803d2f7d8b
ksproska <kamila.sproska@gmail.com>
2021-11-11 18:08:45 +0100

    Add project files.

---
+++ L05/L05_1/L05_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L05_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
