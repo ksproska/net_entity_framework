dd55a50a7f57c220dfee933377e75fc962ff801c
ksproska <kamila.sproska@gmail.com>
2021-11-11 18:36:44 +0100

    init

---
+++ L05/L05_2/L05_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L05_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
