44b5e62cffd15f54be43e8996f2b13863454ef49
ksproska <kamila.sproska@gmail.com>
2021-11-20 14:17:35 +0100

    init 3

--- L06/L06_6/L06_6/Program.cs
+++ L06/L06_6/L06_6/Program.cs
@@ -9,9 +9,35 @@ namespace L06_6
 {
     class Program
     {
+        static (int evenIntegers, int positiveReal, int moreThan5SignStrings, int other) CountMyTypes(params dynamic[] multipleTypes)
+        {
+            (int evenIntegers, int positiveReal, int moreThan5SignStrings, int other) counterTuple = (0, 0, 0, 0);
+
+            foreach(dynamic line in multipleTypes)
+            {
+                switch (line)
+                {
+                    case double f when f >= 0:
+                        counterTuple.positiveReal += 1;
+                        break;
+                    case int i when i % 2 == 0:
+                        counterTuple.evenIntegers += 1;
+                        break;
+                    case string s when s.Length >= 5:
+                        counterTuple.moreThan5SignStrings += 1;
+                        break;
+                    default:
+                        counterTuple.other += 1;
+                        break;
+                }
+            }
+            return counterTuple;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var namedTuple = CountMyTypes("a", "abcde", 4, 4, 3, 2.3, -1.2, 23.8278479873);
+            Console.WriteLine($"(int evenIntegers, int positiveReal, int moreThan5SignStrings, int other): {namedTuple}");
+
         }
     }
 }
