0365160b6d1a72c49417d5e5e25ea625aa60d89b
ksproska <kamila.sproska@gmail.com>
2021-11-25 22:22:41 +0100

    init

---
+++ L07/L07_3/L07_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L07_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
