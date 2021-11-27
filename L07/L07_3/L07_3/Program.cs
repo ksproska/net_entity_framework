5b8c4f27428c3b02e7382e0925a75db5c5b2a69e
ksproska <kamila.sproska@gmail.com>
2021-11-27 21:47:21 +0100

    init

--- L07/L07_3/L07_3/Program.cs
+++ L07/L07_3/L07_3/Program.cs
@@ -2,11 +2,35 @@
 
 namespace L07_3
 {
+    public static class StringExtended
+    {
+        public static string ChangeCaseEverySecond(this string str)
+        {
+            char[] charTab = str.ToCharArray();
+            for(int i=0; i<charTab.Length; i++)
+            {
+                if (!char.IsLetter(charTab[i]))
+                {
+                    charTab[i] = '.';
+                }
+                else if(i%2 == 0)
+                {
+                    charTab[i] = char.ToUpper(charTab[i]);
+                }
+                else
+                {
+                    charTab[i] = char.ToLower(charTab[i]);
+                }
+            }
+            return string.Join("", charTab);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string str = "some text 124 to change";
+            Console.WriteLine(StringExtended.ChangeCaseEverySecond(str));
         }
     }
 }
