3bbbd3cf4410bfa659e2153c1bb107ecdae49934
ksproska <kamila.sproska@gmail.com>
2021-12-23 11:27:39 +0100

    db context created

---
+++ L10/L10_2/L10_2/Data/ShopDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using L10_2.Models;
+using L10_2.ViewModels;
+
+namespace L10_2.Data
+{
+    public class ShopDbContext: DbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options):base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Article> Articles { get; set; }
+    }
+}
--- L10/L10_2/L10_2/ViewModels/Article.cs
+++ L10/L10_2/L10_2/ViewModels/Article.cs
@@ -3,10 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace L10_2.ViewModels
 {
     public class Article
     {
+        [NotMapped]
+        public static string UPLOAD = "upload";
+        [NotMapped]
+        public static string DefaultImage = "/image/no_image.jpg";
+        
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "To short article name")]
+        [MaxLength(20, ErrorMessage = " To long article name, do not exceed {1}")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0.0, double.MaxValue, ErrorMessage = "Please enter a value bigger than {1}")]
+        [DataType(DataType.Currency)]
+        public double Price { get; set; }
+
+        public Category Category { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+
+        [NotMapped]
+        public IFormFile FormFile { get; set; }
+
+        public string ImageFilename { get; set; }
+
+        public string PhotoRelativePath
+        {
+            get { return !ImageFilename.Equals("") ? Path.Combine("\\" + UPLOAD, ImageFilename) : DefaultImage; }
+        }
+
+        public Article() { }
+        public Article(int id, string name, double price, Category category, string imageFilename)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Price = price;
+            this.Category = category;
+            this.ImageFilename = imageFilename;
+        }
+
+        public Article(string name, double price, Category category, string imageFilename)
+            : this(-1, name, price, category, imageFilename) { }
+
+        public Article(int id, string name, double price, Category category) 
+            : this(id, name, price, category, "") { }
+
+        public Article(string name, double price, Category category)
+            : this(-1, name, price, category, "") { }
     }
 }
