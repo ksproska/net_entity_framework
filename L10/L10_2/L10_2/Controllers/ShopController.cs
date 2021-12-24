bf33c7ef52a34a852d8ef10c4ce715483cc8ec47
ksproska <kamila.sproska@gmail.com>
2021-12-24 22:46:19 +0100

    shop links work

---
+++ L10/L10_2/L10_2/Controllers/ShopController.cs
@@ -0,0 +1,35 @@
+﻿using L10_2.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using L10_2.ViewModels;
+
+namespace L10_2.Controllers
+{
+    public class ShopController : Controller
+    {
+        ShopDbContext _context;
+        public ShopController(ShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var shopContextAll = _context.Article.Include(a => a.Category);
+            return View(await shopContextAll.ToListAsync());
+        }
+
+        [Route("Shop/{CategoryId}")]
+        public async Task<IActionResult> Filtered(int CategoryId)
+        {
+            var shopContextFiltered = _context.Article
+                .Where<Article>(item => item.CategoryId == CategoryId)
+                .Include(a => a.Category);
+            return View("Index", await shopContextFiltered.ToListAsync());
+        }
+    }
+}
