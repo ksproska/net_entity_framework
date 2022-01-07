019f00d53b8df9fb8cdb641caa37a5187bc2232b
ksproska <kamila.sproska@gmail.com>
2022-01-07 21:13:38 +0100

    all setup before database

--- L12/L12/L12/Startup.cs
+++ L12/L12/L12/Startup.cs
@@ -1,3 +1,4 @@
+using L12.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace L12
 {
@@ -24,6 +26,8 @@ namespace L12
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddDbContextPool<ShopDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("ShopDb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
