fd33d8af3a7ae7fc45cbb71ff1aba75eb38fe738
ksproska <kamila.sproska@gmail.com>
2022-01-07 20:57:28 +0100

    models and context copied

---
+++ L12/L12/L12/Data/ShopDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using L12.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace L12.Data
+{
+    public class ShopDbContext : DbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options) { }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Article> Article { get; set; }
+    }
+}
---
+++ L12/L12/L12/Models/Category.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace L12.Models
+{
+    public class Category
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "To short category name")]
+        [MaxLength(20, ErrorMessage = " To long category name, do not exceed {1}")]
+        public string Name { get; set; }
+
+        public Category() { }
+
+        public Category(string name)
+        {
+            this.Name = name;
+        }
+        public Category(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
