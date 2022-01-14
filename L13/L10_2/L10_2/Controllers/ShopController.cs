ec356a7290fd86d28a6485879172073ceb02eb9d
ksproska <kamila.sproska@gmail.com>
2022-01-14 22:18:21 +0100

    L13 init

---
+++ L13/L10_2/L10_2/Controllers/ShopController.cs
@@ -0,0 +1,111 @@
+﻿using L10_2.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using L10_2.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+
+namespace L10_2.Controllers
+{
+    public class ShopController : Controller
+    {
+        ShopDbContext _context;
+        private readonly ILogger<ShopController> _logger;
+        public ShopController(ShopDbContext context, ILogger<ShopController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var shopContextAll = _context.Article.Include(a => a.Category);
+            ViewData["CategoryList"] = new SelectList(_context.Category, "Id", "Name");
+            //ViewData["SelectedId"] = -1;
+            return View(await shopContextAll.ToListAsync());
+        }
+
+        [Route("Shop/{CategoryId}")]
+        public async Task<IActionResult> Filtered(int CategoryId)
+        {
+            var shopContextFiltered = _context.Article
+                .Where<Article>(item => item.CategoryId == CategoryId)
+                .Include(a => a.Category);
+            ViewData["CategoryList"] = new SelectList(_context.Category, "Id", "Name");
+            //ViewData["SelectedId"] = CategoryId;
+            return View("Index", await shopContextFiltered.ToListAsync());
+        }
+
+        public async Task<IActionResult> AddCartInShop(int? id)
+        {
+            string sCount = Request.Cookies[id.ToString()];
+            int iCount = 0;
+            if (sCount != null)
+            {
+                iCount = int.Parse(sCount);
+            }
+            iCount += 1;
+            //ViewData["ExtraMessage"] = "Product added to cart";
+            Response.Cookies.Append(id.ToString(), iCount.ToString());
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> AddCart(int? id)
+        {
+            string sCount = Request.Cookies[id.ToString()];
+            int iCount = 0;
+            if (sCount != null)
+            {
+                iCount = int.Parse(sCount);
+            }
+            iCount += 1;
+
+            Response.Cookies.Append(id.ToString(), iCount.ToString());
+            return RedirectToAction("ShoppingCart");
+        }
+
+        public async Task<IActionResult> SubCart(int? id)
+        {
+            string sCount = Request.Cookies[id.ToString()];
+            int iCount = 1;
+            iCount = int.Parse(sCount);
+            iCount -= 1;
+            
+            if(iCount > 0)
+            {
+                Response.Cookies.Append(id.ToString(), iCount.ToString());
+            }
+            else
+            {
+                Response.Cookies.Delete(id.ToString());
+            }
+            return RedirectToAction("ShoppingCart");
+        }
+
+        public async Task<IActionResult> DelCart(int? id)
+        {
+            Response.Cookies.Delete(id.ToString());
+            return RedirectToAction("ShoppingCart");
+        }
+
+        [Route("Shop/ShoppingCart")]
+        public async Task<IActionResult> ShoppingCart()
+        {
+            var allCartIds = Request.Cookies.Select(item => item.Key).ToList();
+
+            var allCartArticles = _context.Article.Include(a => a.Category)
+                .Where<Article>(item => allCartIds.Contains(item.Id.ToString()))
+                .Select(item => new CartArticle(item, Request.Cookies[item.Id.ToString()]));
+
+            if(allCartArticles.ToList().Count == 0)
+            {
+                return View("ShoppingCartEmpty");
+            }
+            return View(await allCartArticles.ToListAsync());
+        }
+    }
+}
---
+++ L13/L10_2/L10_2/Startup.cs
@@ -0,0 +1,74 @@
+using L10_2.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace L10_2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(5);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
+            services.AddControllersWithViews();
+            services.AddDbContextPool<ShopDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("ShopDb")));
+            // todo check if necessery:
+            // services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseSession();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
